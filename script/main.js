5548a6f29898f623217cb44df1828182fc217c30
Geruco <loquet.gregor@gmail.com>
2019-09-27 21:52:02 -0400

    v1

--- script/main.js
+++ script/main.js
@@ -24,6 +24,7 @@ window.addEventListener("load", function(){
         
         document.getElementById(id).classList.toggle("hidden");
     }
+    
     if(DEBUG == false)
         initKonami();
     initButtons();
